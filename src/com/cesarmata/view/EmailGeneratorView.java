8c94a89e4f219400a51fb4bebe023f9d30ecac00
César <dev.pcesarmata@gmail.com>
2020-08-11 21:51:50 -0500

    Se actualizan versiones entre branches

--- src/com/cesarmata/view/EmailGeneratorView.java
+++ src/com/cesarmata/view/EmailGeneratorView.java
@@ -7,11 +7,16 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JInternalFrame;
 
 import com.cesarmata.model.Nota;
 import com.cesarmata.service.EmailGeneratorService;
 
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 /**
  * @author Cesar Mata.
  *
@@ -33,7 +38,9 @@ public class EmailGeneratorView {
 	private JTextField txtContenido;
 	private JTextField txtTirajes;
 	private JTextField textField;
-
+	private JScrollPane sclBancos;
+	private JTable tblNota;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -63,113 +70,170 @@ public class EmailGeneratorView {
 	private void initialize() {
 		frmGeneradorHtml = new JFrame();
 		frmGeneradorHtml.setTitle("Generador html");
-		frmGeneradorHtml.setBounds(100, 100, 610, 683);
+		frmGeneradorHtml.setBounds(100, 100, 812, 794);
 		frmGeneradorHtml.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmGeneradorHtml.getContentPane().setLayout(null);
 		
 		JLabel lblCliente = new JLabel("* Cliente:");
-		lblCliente.setBounds(51, 40, 69, 16);
+		lblCliente.setBounds(50, 13, 69, 16);
 		frmGeneradorHtml.getContentPane().add(lblCliente);
 		
 		txtCliente = new JTextField();
-		txtCliente.setBounds(195, 37, 298, 22);
+		txtCliente.setBounds(195, 10, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtCliente);
 		txtCliente.setColumns(10);
 		
+		// ------------------------------------------------ //
+		
+		// - - - - - Botón para agregar el texto al cuerpo  - - - - - //
+		JButton btnAgregar = new JButton("Agregar");
+		btnAgregar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				agregar();
+			}
+		});
+		btnAgregar.setBounds(396, 363, 97, 25);
+		frmGeneradorHtml.getContentPane().add(btnAgregar);
+		
+		// - - - - - Botón para generar el archivo - - - - - //
 		JButton btnGenerarArchivo = new JButton("Generar archivo");
 		btnGenerarArchivo.addActionListener(new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				generarArchivo();
 			}
 		});
-		btnGenerarArchivo.setBounds(405, 578, 141, 25);
+		btnGenerarArchivo.setBounds(609, 695, 141, 25);
 		frmGeneradorHtml.getContentPane().add(btnGenerarArchivo);
 		
+		// ------------------------------------------------ //
+		
 		JLabel lblEncabezado = new JLabel("* Encabezado:");
-		lblEncabezado.setBounds(50, 82, 88, 16);
+		lblEncabezado.setBounds(50, 42, 88, 16);
 		frmGeneradorHtml.getContentPane().add(lblEncabezado);
 		
 		txtEncabezado = new JTextField();
-		txtEncabezado.setBounds(195, 76, 298, 22);
+		txtEncabezado.setBounds(195, 39, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtEncabezado);
 		txtEncabezado.setColumns(10);
 		
 		JLabel lblFuente = new JLabel("* Fuente:");
-		lblFuente.setBounds(51, 126, 56, 16);
+		lblFuente.setBounds(50, 75, 56, 16);
 		frmGeneradorHtml.getContentPane().add(lblFuente);
 		
 		txtFuente = new JTextField();
-		txtFuente.setBounds(195, 123, 298, 22);
+		txtFuente.setBounds(195, 72, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtFuente);
 		txtFuente.setColumns(10);
 		
 		JLabel lblSeccion = new JLabel("* Secci\u00F3n:");
-		lblSeccion.setBounds(51, 176, 87, 16);
+		lblSeccion.setBounds(50, 110, 87, 16);
 		frmGeneradorHtml.getContentPane().add(lblSeccion);
 		
 		txtSeccion = new JTextField();
-		txtSeccion.setBounds(195, 173, 298, 22);
+		txtSeccion.setBounds(195, 107, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtSeccion);
 		txtSeccion.setColumns(10);
 		
 		JLabel lblAutor = new JLabel("* Autor:");
-		lblAutor.setBounds(51, 226, 56, 16);
+		lblAutor.setBounds(50, 145, 56, 16);
 		frmGeneradorHtml.getContentPane().add(lblAutor);
 		
 		txtAutor = new JTextField();
-		txtAutor.setBounds(195, 223, 298, 22);
+		txtAutor.setBounds(195, 142, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtAutor);
 		txtAutor.setColumns(10);
 		
 		JLabel lblContenido = new JLabel("* Contenido:");
-		lblContenido.setBounds(50, 411, 88, 16);
+		lblContenido.setBounds(50, 250, 88, 16);
 		frmGeneradorHtml.getContentPane().add(lblContenido);
 		
 		txtContenido = new JTextField();
-		txtContenido.setBounds(195, 408, 298, 22);
+		txtContenido.setBounds(195, 247, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtContenido);
 		txtContenido.setColumns(10);
 		
 		JLabel lblUrlNota = new JLabel("* Link de la nota:");
-		lblUrlNota.setBounds(54, 465, 113, 16);
+		lblUrlNota.setBounds(50, 285, 113, 16);
 		frmGeneradorHtml.getContentPane().add(lblUrlNota);
 		
 		txtUrlFuente = new JTextField();
-		txtUrlFuente.setBounds(195, 462, 298, 22);
+		txtUrlFuente.setBounds(195, 282, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtUrlFuente);
 		txtUrlFuente.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Link imagen:");
-		lblNewLabel.setBounds(51, 524, 87, 16);
+		lblNewLabel.setBounds(50, 320, 87, 16);
 		frmGeneradorHtml.getContentPane().add(lblNewLabel);
 		
 		txtLinkImagen = new JTextField();
-		txtLinkImagen.setBounds(195, 521, 298, 22);
+		txtLinkImagen.setBounds(195, 317, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtLinkImagen);
 		txtLinkImagen.setColumns(10);
 		
 		JLabel lblTirajes = new JLabel("Tiraje o impactos:");
-		lblTirajes.setBounds(51, 294, 116, 16);
+		lblTirajes.setBounds(51, 180, 116, 16);
 		frmGeneradorHtml.getContentPane().add(lblTirajes);
 		
 		txtTirajes = new JTextField();
-		txtTirajes.setBounds(195, 291, 298, 22);
+		txtTirajes.setBounds(195, 177, 298, 22);
 		frmGeneradorHtml.getContentPane().add(txtTirajes);
 		txtTirajes.setColumns(10);
 		
 		JLabel lblAdEquivalence = new JLabel("Ad Equivalence:");
-		lblAdEquivalence.setBounds(51, 365, 102, 16);
+		lblAdEquivalence.setBounds(50, 215, 102, 16);
 		frmGeneradorHtml.getContentPane().add(lblAdEquivalence);
 		
 		textField = new JTextField();
-		textField.setBounds(195, 362, 298, 22);
+		textField.setBounds(195, 212, 298, 22);
 		frmGeneradorHtml.getContentPane().add(textField);
 		textField.setColumns(10);
 		
+		sclBancos = new JScrollPane();
+		sclBancos.setBounds(66, 406, 684, 258);
+		frmGeneradorHtml.getContentPane().add(sclBancos);
+		
+		tblNota = new JTable();
+		tblNota.setModel(new DefaultTableModel(
+			new String[][] {
+				{"Cliente", null, null, null},
+				{"Encabezado", "Fuente", "Sección", "Autor"},
+				{"Tiraje", "Ad equivalence", "Contenido", null},
+				{"Link nota", null, null, null},
+				{"Link imagen", null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"-", "-", "-", "-"
+			}
+		));
+		tblNota.getColumnModel().getColumn(0).setPreferredWidth(251);
+		tblNota.getColumnModel().getColumn(1).setPreferredWidth(185);
+		tblNota.getColumnModel().getColumn(2).setPreferredWidth(194);
+		tblNota.getColumnModel().getColumn(3).setPreferredWidth(148);
+		sclBancos.setViewportView(tblNota);
+	}
+	
+	public void agregar() {
+		tblNota.setValueAt(txtCliente.getText(), 0, 0);
+		
+		tblNota.setValueAt(txtEncabezado.getText(), 1, 0);
+		tblNota.setValueAt(txtFuente.getText(), 1, 1);
+		tblNota.setValueAt(txtSeccion.getText(), 1, 2);
+		tblNota.setValueAt(txtAutor.getText(), 1, 3);
+		
+		tblNota.setValueAt("tiraje", 2, 0);
+		tblNota.setValueAt("Ad. Equivalence", 2, 1);
+		tblNota.setValueAt(txtContenido.getText(), 2, 2);
+		
+		tblNota.setValueAt(txtUrlFuente.getText(), 3, 0);
+		
+		tblNota.setValueAt(txtLinkImagen.getText(), 4, 0);
 	}
 	
 	public void generarArchivo () {
-		nota.setCliente(txtCliente.getText()); 
+		nota.setCliente(txtCliente.getText());
 		nota.setEncabezado(txtEncabezado.getText());
 		nota.setSeccion(txtSeccion.getText());
 		nota.setFuente(txtFuente.getText());
